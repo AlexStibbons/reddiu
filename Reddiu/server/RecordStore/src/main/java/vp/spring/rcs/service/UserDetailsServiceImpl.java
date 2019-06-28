cf2aced5c627e5f4b7ed612b65467517604ec689
AlexStibbons <pantic.aleks@gmail.com>
2019-06-28 17:06:07 +0200

    add register user to backend

--- Reddiu/server/RecordStore/src/main/java/vp/spring/rcs/service/UserDetailsServiceImpl.java
+++ Reddiu/server/RecordStore/src/main/java/vp/spring/rcs/service/UserDetailsServiceImpl.java
@@ -11,16 +11,26 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import vp.spring.rcs.data.AuthorityRepository;
 import vp.spring.rcs.data.UserRepository;
+import vp.spring.rcs.model.user.SecurityAuthority;
 import vp.spring.rcs.model.user.SecurityUser;
+import vp.spring.rcs.model.user.SecurityUserAuthority;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
   @Autowired
   private UserRepository userRepository;
+  
+  @Autowired
+  AuthorityRepository authorityRepository;
+  
+  @Autowired
+  PasswordEncoder passwordEncoder;
 
   @Override
   @Transactional
@@ -46,7 +56,44 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	}
   }
   
+  public SecurityUser register(SecurityUser securityUser, List<String> roles) {
+		if (userRepository.findByUsername(securityUser.getUsername()) != null) {
+			return null; // ako vec postoji korisnik sa tim korisnickim imenom
+		} else {
+			// objekat korisnik vezemo sa ulogama
+
+			// uloge su predstavljene objektom SecurityAuthority
+			// za svaku ulogu, nadjemo odgovarajuci SecurityAuthority objekat
+			for (String role : roles) {
+				SecurityAuthority authority = authorityRepository
+						.findByName(role);
+
+				// povezemo authority sa korisnikom preko objekta
+				// SecurityUserAUthority
+				SecurityUserAuthority sua = new SecurityUserAuthority();
+				sua.setAuthority(authority);
+				sua.setUser(securityUser);
+				// dodamo vezni objekat u korisnikovu listu koju ce biti
+				// kaskadno perzistirana
+				// zajedno sa korisnikom
+				securityUser.getUserAuthorities().add(sua);
+			}
+			// enkodujemo sifru pre snimanja u bazu
+			securityUser.setPassword(passwordEncoder.encode(securityUser.getPassword()));
+			
+			return userRepository.save(securityUser); // perzistira se korisnik
+		}
+  }
+  
   public SecurityUser findByUserName(String username) {
 	  return userRepository.findByUsername(username);
   }
+  
+  public List<SecurityAuthority> findAllAuthorities() {
+	  return authorityRepository.findAll();
+  }
+  
+
+ 
+  
 }
--- Reddiu/server/RecordStore/src/main/java/vp/spring/rcs/web/controller/UserController.java
+++ Reddiu/server/RecordStore/src/main/java/vp/spring/rcs/web/controller/UserController.java
@@ -3,20 +3,23 @@ package vp.spring.rcs.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vp.spring.rcs.model.user.SecurityUser;
 import vp.spring.rcs.security.TokenUtils;
+import vp.spring.rcs.service.UserDetailsServiceImpl;
 import vp.spring.rcs.web.dto.LoginDTO;
 import vp.spring.rcs.web.dto.TokenDTO;
+import vp.spring.rcs.web.dto.UserDTO;
 
 @RestController
 public class UserController {
@@ -25,7 +28,7 @@ public class UserController {
 	AuthenticationManager authenticationManager;
 	
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private UserDetailsServiceImpl userDetailsService;
 	
 	@Autowired
 	TokenUtils tokenUtils;
@@ -44,6 +47,23 @@ public class UserController {
         } catch (Exception ex) {
             return new ResponseEntity<TokenDTO>(new TokenDTO(""), HttpStatus.BAD_REQUEST);
         }
+	}
+	
+	@RequestMapping(value = "/api/register", method = RequestMethod.POST)
+	public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
+        SecurityUser user = new SecurityUser();
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        
+        user = userDetailsService.register(user, userDTO.getRoles());
+        
+        if (user != null) {
+        	return new ResponseEntity<>(new UserDTO(user), HttpStatus.CREATED); 
+        } else { // ako vec postoji korisnik sa tim korisnickim imenom
+        	return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }        
 	}
 
 	/*@RequestMapping(value = "/api/shopping-cart", method = RequestMethod.GET)
@@ -78,4 +98,6 @@ public class UserController {
         	return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }        
 	}*/
+	
+	
 }
